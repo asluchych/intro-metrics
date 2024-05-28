8e29c486b7cc606278e98c289b4d49c767263078
asluchych <95502501+asluchych@users.noreply.github.com>
2024-05-28 14:17:48 +0200

    Correct assignment parts ordering and small adjustments

--- Chapter05/ComputerTutorial5.R
+++ Chapter05/ComputerTutorial5.R
@@ -8,7 +8,7 @@
 ##### Assignment 3: House Prices in the Multiple Regression Model #####
 
 # load data
-houses <- read.csv("houseprices2.csv")
+houses <- read.csv('houseprices2.csv')
 
 # data exploration
 summary(houses) # data summary
@@ -16,13 +16,13 @@ cor(houses) # data correlation matrix
 # scatterplot price vs. sqft
 plot(PRICE ~ SQFT, data = houses)
 # scatterplot price vs. age
-plot(PRICE ~ AGE, data = houses)
+boxplot(PRICE ~ AGE, data = houses)
 
 # a) model 1
 regA <- lm(PRICE ~ SQFT + AGE, data = houses)
 summary(regA)
 
-# b) 95% Confidence Interval
+# b) 95% confidence interval
 # get coefficients
 co <- coef(summary(regA))
 # calculate upper and lower bound 
@@ -36,35 +36,35 @@ tstatA <- (co[3,1] - (-1000))/co[3,2]
 pValA <- pt(tstatA, df = regA$df.residual)
 
 # d) model 2: include squared terms
-regB <- lm(PRICE ~ SQFT + I(SQFT^2) + AGE + I(AGE^2), data = houses)
-summary(regB)
+regD <- lm(PRICE ~ SQFT + I(SQFT^2) + AGE + I(AGE^2), data = houses)
+summary(regD)
 # get coefficients
-coB <- coef(summary(regB))
-# e) marginal effects
-effectSqftMin <- coB[2,1] + 2*coB[3,1]*min(houses$SQFT)
-effectSqftMax <- coB[2,1] + 2*coB[3,1]*max(houses$SQFT)
-effectSqft2000 <- coB[2,1] + 2*coB[3,1]*2000
+coD <- coef(summary(regD))
+# marginal effects sqft
+effectSqftMin <- coD[2,1] + 2*coD[3,1]*min(houses$SQFT)
+effectSqftMax <- coD[2,1] + 2*coD[3,1]*max(houses$SQFT)
+effectSqft2000 <- coD[2,1] + 2*coD[3,1]*2000
 
-# f) marginal effects
-effectAgeMin <- coB[4,1] + 2*coB[5,1]*min(houses$AGE)
-effectAgeMax <- coB[4,1] + 2*coB[5,1]*max(houses$AGE)
-effectAge25 <- coB[4,1] + 2*coB[5,1]*25
+# e) marginal effects age
+effectAgeMin <- coD[4,1] + 2*coD[5,1]*min(houses$AGE)
+effectAgeMax <- coD[4,1] + 2*coD[5,1]*max(houses$AGE)
+effectAge25 <- coD[4,1] + 2*coD[5,1]*25
 
-# g) 95% confidence interval for a house with 2000 sqft
-covB <- vcov(regB) # variance - covariance matrix of the coefficients
+# f) 95% confidence interval for a house with 2000 sqft
+covD <- vcov(regD) # variance - covariance matrix of the coefficients
 # calculate standar derror of the linear combination of coefficients
-seSqft2000 <- sqrt(covB[2,2] + 4000^2*covB[3,3] + 2*4000*covB[2,3])
+seSqft2000 <- sqrt(covD[2,2] + 4000^2*covD[3,3] + 2*4000*covD[2,3])
 # calculate upper and lower bound
-upB <- effectSqft2000 + qt(0.975, regB$df.residual)*seSqft2000
-lowB <- effectSqft2000 - qt(0.975, regB$df.residual)*seSqft2000
+upD <- effectSqft2000 + qt(0.975, regD$df.residual)*seSqft2000
+lowD <- effectSqft2000 - qt(0.975, regD$df.residual)*seSqft2000
 
-# f) t-Test H_O: effect >=-1000 at age=25
-# calculate standarderror of the linear combination of coefficients
-seAge25 <- sqrt(covB[4,4] + (2*25)^2*covB[5,5] + 2*(2*25)*covB[4,5])
+# g) t-Test H_O: effect >=-1000 at age=25
+# calculate standard error of the linear combination of coefficients
+seAge25 <- sqrt(covD[4,4] + (2*25)^2*covD[5,5] + 2*(2*25)*covD[4,5])
 # calculate t-Statistic: H_0: beta_4 + 2*beta_5*25 >= -1000
-tstatB <- (effectAge25 - (-1000))/seAge25
+tstatD <- (effectAge25 - (-1000))/seAge25
 # calculate the p-value, left-sided
-pValB <- pt(tstatB, df = regB$df.residual)
+pValD <- pt(tstatD, df = regD$df.residual)
 
 
 # Extra: graphic evaluation of models
@@ -72,8 +72,8 @@ pValB <- pt(tstatB, df = regB$df.residual)
 # Fitted graphs
 sqft <- seq(20, 8000, 1)
 age <- seq(0, 80, 0.5)
-fittedValsSQFT <- regB$coefficients[1] + regB$coefficients[2]*sqft + regB$coefficients[3]*sqft^2
-fittedValsAGE <- regB$coefficients[1] + regB$coefficients[4]*age + regB$coefficients[5]*age^2
+fittedValsSQFT <- regD$coefficients[1] + regD$coefficients[2]*sqft + regD$coefficients[3]*sqft^2
+fittedValsAGE <- regD$coefficients[1] + regD$coefficients[4]*age + regD$coefficients[5]*age^2
 
 plot(PRICE ~ SQFT, data = houses)
 lines(fittedValsSQFT ~ sqft, col = "red")
@@ -87,9 +87,9 @@ plot(regA$residuals ~ houses$SQFT, main = "Residuals Model 1")
 abline(h=0)
 
 # model 2
-plot(regB$residuals ~ houses$SQFT, main = "Residuals Model 2")
+plot(regD$residuals ~ houses$SQFT, main = "Residuals Model 2")
 abline(h=0)
 
-plot(regB$residuals ~ houses$AGE, main = "Residuals Model 2")
+plot(regD$residuals ~ houses$AGE, main = "Residuals Model 2")
 abline(h=0)
 
