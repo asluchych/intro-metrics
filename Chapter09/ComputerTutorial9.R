a39752481a5b4ad13ea3ad164281d74c16e2f5b8
asluchych <anatolblr@gmail.com>
2024-07-05 13:57:13 +0200

    Minor adjustments

--- Chapter09/ComputerTutorial9.R
+++ Chapter09/ComputerTutorial9.R
@@ -4,9 +4,11 @@
 #                                  Assignment 2                                #
 #                                                                              #
 ################################################################################
-# necessary packages: if not installed, type install.packages(c('lmtest', 'sandwich', 'nlme', 'orcutt')) 
-# in the console and press Enter
-library(lmtest) # for function: bgtest, NeweyWest
+# necessary packages: if not installed, type
+# install.packages(c('lmtest', 'sandwich', 'nlme', 'orcutt')) in the console and
+# press Enter
+
+library(lmtest) # for functions: bgtest, NeweyWest
 library(sandwich) # for function: coeftest
 library(nlme) # for function: gls
 library(orcutt) # for function: cochrane.orcutt
@@ -31,18 +33,18 @@ c(ls_low, ls_up)
 acf(regA$residuals)
 pacf(regA$residuals)
 
-# c) Breusch-Godfrey Test for First-Order Autocorrelation
-# save residuals to use as dependent variable in auxiliary regression
+# c) Breusch-Godfrey Test
+# save residuals to use as dependent variable in the auxiliary regression
 vacancies$resid <- regA$residuals
 # save lagged residuals to use as explanatory variable in auxiliary regression
-vacancies$resid_lag <- c(0, regA$residuals[1:nrow(vacancies)-1]) # as default in EViews: 
-# missing value lagged residuals set to zero.			
+vacancies$resid_lag <- c(0, regA$residuals[1:nrow(vacancies)-1]) # as default in
+# EViews, missing value lagged residuals are set to zero			
 # fit auxiliary regression
 aux <- lm(resid ~ log(u) + resid_lag, data = vacancies)
 summary(aux)
 # save summary of the auxiliary regression
 sumAux <- summary(aux)
-# calculate test statistic - LM test -> N*R^2
+# calculate test statistic - LM test: N*R^2
 bg <- nrow(vacancies)*sumAux$r.squared
 # calculate p-value ->  LM test is chi-squared distributed
 pVal <- 1 - pchisq(bg, 1)
@@ -52,15 +54,16 @@ bgtest(regA, order = 1)
 # d) OLS with HAC standard errors
 # 95% confidence interval for beta_2
 # get coefficients
-co_hac <- coeftest(regA, vcov = NeweyWest(regA, lag = 3, prewhite = FALSE, adjust = TRUE)) # as default in EViews
+co_hac <- coeftest(regA, vcov = NeweyWest(regA, lag = 3, prewhite = FALSE, 
+                                          adjust = TRUE)) # as default in EViews
 co_hac
 # calculate upper and lower bound 
 hac_up <- co_hac[2,1] + qt(0.975, regA$df.residual)*co_hac[2,2]
 hac_low <- co_hac[2,1] - qt(0.975, regA$df.residual)*co_hac[2,2]
 c(hac_low, hac_up)
 
-# e) # assumption: errors follow AR(1) error model, Generalized Least Squares Estimation 
-# via Model Transformation
+# e) # assumption: errors follow AR(1) error model, Generalized Least Squares 
+# Estimation via Model Transformation
 # estimate rho via Cochrane-Orcutt Procedure
 rho <- cochrane.orcutt(regA)$rho
 # load data 
@@ -71,12 +74,13 @@ vac_new <- log(vac_new)
 vac_new$jv_lag <- c(NA, vac_new$jv[1:nrow(vac_new)-1]) 
 # lagged log(u)
 vac_new$u_lag <- c(NA, vac_new$u[1:nrow(vac_new)-1]) 
-# dependent variable
+# new dependent variable
 vac_new$y <- vac_new$jv - rho*vac_new$jv_lag 
-# constant instead of 1s
+# new constant instead of 1s
 vac_new$c <- rep(1 - rho, nrow(vac_new))
-# explanatory variable
+# new explanatory variable
 vac_new$x <- vac_new$u - rho*vac_new$u_lag
+# fit the model
 regE <- lm(y ~ -1 + c + x, data = vac_new)
 summary(regE)
 
@@ -93,7 +97,3 @@ glm_low <- co_glm[2,1] - qt(0.975, regA$df.residual)*co_glm[2,2]
 c(glm_low, glm_up)
 c(hac_low, hac_up)
 c(ls_low, ls_up)
-
-# Is the estimate for ρ significantly different from zero at a 5% significance level?
-# regE <- gls(log(jv) ~ log(u), correlation = corAR1(), data = vacancies)
-# summary(regE) 
